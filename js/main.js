016553c7f8724efebb41f96b577fffdc63d2b876
amakom <obiajuluamakom@gmail.com>
2022-08-11 23:49:46 +0100

    Update Commit

--- js/main.js
+++ js/main.js
@@ -121,9 +121,23 @@ responsive: [
 
 
 /*---------------------------------
-theme colors
+accordian
 -----------------------------------*/
-
+$(document).ready(function(){
+    $(".card-header").click(function(){
+        // self clicking close
+        if($(this).next(".card-body").hasClass("active")){
+            $(this).next(".card-body").removeClass("active").slideUp()  
+            $(this).children("span").removeClass("fa-minus").addClass("fa-plus")
+        }
+        else{
+            $(".card .card-body").removeClass("active").slideUp()
+            $(".card .card-header span").removeClass("fa-minus").addClass("fa-plus");
+            $(this).next(".card-body").addClass("active").slideDown()  
+            $(this).children("span").removeClass("fa-plus").addClass("fa-minus")
+        }
+    })
+})
 
 
 /*---------------------------------
